d2395c153be94acf2c4a788876eb66c95dea1dec
JarbasAi <jarbasai@mailfence.com>
2023-05-17 00:05:56 +0100

    packaging

--- scripts/bump_minor.py
+++ scripts/bump_minor.py
@@ -2,7 +2,7 @@ import fileinput
 from os.path import join, dirname
 
 
-version_file = join(dirname(dirname(__file__)), "ovos_PHAL_plugin_shell_companion", "version.py")
+version_file = join(dirname(dirname(__file__)), "ovos_gui_plugin_shell_companion", "version.py")
 version_var_name = "VERSION_MINOR"
 build_var_name = "VERSION_BUILD"
 alpha_var_name = "VERSION_ALPHA"
