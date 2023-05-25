8f1d916cc91bdbbc678343c283736eb7b97f66f2
Daniel McKnight <daniel@neongecko.com>
2023-05-25 11:17:57 -0700

    Fix unit test failures Closes #3

--- test/unittests/test_smartspeaker_extension.py
+++ test/unittests/test_smartspeaker_extension.py
@@ -25,9 +25,11 @@ class TestSmartSpeakerExtension:
                 }
             })
         mock_get.return_value = config
-        smartSpeaker = SmartSpeakerExtension(MessageBusMock(), MessageBusMock())
+        smartSpeaker = SmartSpeakerExtension(config, MessageBusMock(),
+                                             MessageBusMock())
         smartSpeaker.set_backend_type = mock.Mock()
-        message_data = Message("ovos.pairing.set.backend", {'backend': 'unknown'})
+        message_data = Message("ovos.pairing.set.backend",
+                               {'backend': 'unknown'})
         smartSpeaker.set_backend_type(message_data)
         smartSpeaker.set_backend_type.assert_any_call(message_data)
 
@@ -41,7 +43,8 @@ class TestSmartSpeakerExtension:
                 }
             })
         mock_get.return_value = config
-        smartSpeaker = SmartSpeakerExtension(MessageBusMock(), MessageBusMock())
+        smartSpeaker = SmartSpeakerExtension(config, MessageBusMock(),
+                                             MessageBusMock())
         smartSpeaker.start_homescreen_process = mock.Mock()
         message_data = Message("ovos.pairing.process.completed", {})
         smartSpeaker.start_homescreen_process(message_data)
