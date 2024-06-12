5b6a0006009fe1d7951cc288b381f4a3a607ed94
Daniel McKnight <daniel@neon.ai>
2024-06-12 13:40:38 -0700

    Remove `wallpaper_rotation` session data that is handled in Qt already

--- ovos_gui_plugin_shell_companion/__init__.py
+++ ovos_gui_plugin_shell_companion/__init__.py
@@ -78,8 +78,6 @@ class OVOSShellCompanionExtension(GUIExtension):
         self.gui.register_handler("mycroft.device.settings.factory", self.handle_device_display_factory)
 
         # Display settings
-        self.gui.register_handler("speaker.extension.display.set.wallpaper.rotation",
-                                  self.handle_display_wallpaper_rotation_config_set)
         self.gui.register_handler("speaker.extension.display.set.auto.dim",
                                   self.handle_display_auto_dim_config_set)
         self.gui.register_handler("speaker.extension.display.set.auto.nightmode",
@@ -146,10 +144,7 @@ class OVOSShellCompanionExtension(GUIExtension):
     def handle_device_display_settings(self, message):
         LOG.debug(f"Display settings: {self.local_display_config}")
         self.gui['state'] = 'settings/display_settings'
-
-        # TODO: Refactor below to query PHAL plugin
-        self.gui['display_wallpaper_rotation'] = self.local_display_config.get("wallpaper_rotation", False)
-
+        # wallpaper_rotation data is determined via Messagebus in Qt directly
         self.gui['display_auto_dim'] = self.local_display_config.get("auto_dim", False)
         self.gui['display_auto_nightmode'] = self.local_display_config.get("auto_nightmode", False)
         self.gui.show_page("SYSTEM_AdditionalSettings.qml", override_idle=True)
@@ -161,16 +156,6 @@ class OVOSShellCompanionExtension(GUIExtension):
         self.gui['state'] = 'settings/about_page'
         self.gui['system_info'] = system_information
         self.gui.show_page("SYSTEM_AdditionalSettings.qml", override_idle=True)
-
-    def handle_display_wallpaper_rotation_config_set(self, message: GUIMessage):
-        """
-        Handle a GUI event requesting a new wallpaper rotation setting
-        @param message: `speaker.extension.display.set.wallpaper.rotation`
-        """
-        # TODO: This should come from the PHAL plugin, not a separate config
-        wallpaper_rotation = message.data.get("wallpaper_rotation", False)
-        self.local_display_config["wallpaper_rotation"] = wallpaper_rotation
-        self.local_display_config.store()
 
     def handle_display_auto_dim_config_set(self, message):
         auto_dim = message.data.get("auto_dim", False)
